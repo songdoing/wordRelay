ee47f5725ee33f2dac62ac6a6370b531261160a2
wonjinsong <msmk22239@gmail.com>
2020-11-13 01:04:00 -0500

    webpack setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,30 @@
+const path = require('path');
+
 module.exports = {
+    name : 'wordrelay-dev',
+    mode : 'development',
+    devtool : 'eval',
+    resolve : {
+        extensions: ['.js', '.jsx']
+    },
     
-}
+    entry: {
+        app: ['./client'],
+    },
+
+    module: {
+        rules: [{
+            test: /\.jsx?$/,
+            loader : 'babel-loader',
+            options : {
+                presets : ['@babel/preset-env', '@babel/preset-react'],
+                plugins : ['@babel/plugin-proposal-class-properties'],
+            },
+        }],
+    },
+
+    output: {
+        path : path.join(__dirname, 'dist'),
+        filename : 'app.js'
+    },
+};
--- wordRelayClass.jsx
+++ wordRelayClass.jsx
@@ -3,36 +3,13 @@ const { Component } = React;
 
 class WordRelay extends Component {
     state = {
-        word: 'hello',
-        value: '',
-        result: ''
+        text : 'Hello webpack',
     };
 
-    onSubmitForm = (e) => {
     
-    }
-
-    onChangeInput = (e) => {
-
-    }
-
-    input;
-
-    onRefInput = (ref) => {
-        this.input = ref;
-    };
 
     render() {
-        return (
-            <>
-                <div>{this.state.word}</div>
-                <form onSubmit = {this.onSubmitForm}>
-                    <input ref={this.onRefInput} value={this.state.value} onChange= {this.onChangeInput} />
-                    <button>Enter</button>
-                </form>
-                <div>{this.state.result}</div>
-            </>
-        );
+        return <h1>{this.state.text}</h1>;
     }
 }
 
