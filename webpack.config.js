975fe86ce545432536183bbb36dfb69e8a87c90c
wonjinsong <msmk22239@gmail.com>
2020-12-30 18:11:50 -0500

    wordRelayClass

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,32 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    name : 'wordrelay-dev',
-    mode : 'development',
-    devtool : 'eval',
-    resolve : {
-        extensions: ['.js', '.jsx']
-    },
-    
-    entry: {
-        app: ['./client'],
-    },
+  name: "wordrelay-dev",
+  mode: "development",
+  devtool: "eval",
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
 
-    module: {
-        rules: [{
-            test: /\.jsx?$/,
-            loader : 'babel-loader',
-            options : {
-                presets : ['@babel/preset-env', '@babel/preset-react'],
-                plugins : ['@babel/plugin-proposal-class-properties'],
-            },
-        }],
-    },
+  entry: {
+    app: ["./client"],
+  },
 
-    output: {
-        path : path.join(__dirname, 'dist'),
-        filename : 'app.js'
-    },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+          plugins: ["@babel/plugin-proposal-class-properties"],
+        },
+      },
+    ],
+  },
+
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "app.js",
+  },
 };
--- wordRelayClass.jsx
+++ wordRelayClass.jsx
@@ -3,13 +3,50 @@ const { Component } = React;
 
 class WordRelay extends Component {
     state = {
-        text : 'Hello webpack',
+        word : 'jenny',
+        value : '',
+        result : '',
     };
 
-    
+    onSubmitForm = (e) => {
+        e.preventDefault();
+        if(this.state.word[this.state.word.length - 1] === this.state.value[0]) {
+            this.setState({
+                result : 'Good job!',
+                word : this.state.value,
+                value : '',
+            });
+            this.input.focus();
+        } else {
+            this.setState({
+                result : 'Try again.',
+                value : '',
+            });
+            this.input.focus();
+        }
+    };
+
+    onChangeInput = (e) => {
+        this.setState( { value : e.target.value });
+    };
+
+    input;
+
+    onRefInput = (c) => {
+        this.input= c;
+    };    
 
     render() {
-        return <h1>{this.state.text}</h1>;
+        return (
+            <>
+                <div>{this.state.word}</div>
+                <form onSubmit = {this.onSubmitForm}>
+                    <input ref={this.onRefInput} value={this.state.value} onChange={this.onChangeInput} />
+                    <button>ENTER</button>
+                </form>
+                <div>{this.state.result}</div>
+            </>
+        );
     }
 }
 
